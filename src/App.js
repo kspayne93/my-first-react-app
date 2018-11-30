d0e0577cc0ff0d3a5dda81f085182fa028616ca7
kspayne93 <kyle@investavante.com>
2018-11-29 23:24:37 -0700

    updated text to be soy el gato

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-          Hello World! 
+          Welcome to my project
           </p>
           <a
             className="App-link"
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Soy el guapo.
           </a>
         </header>
       </div>
